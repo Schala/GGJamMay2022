fe7cd9b23421b8359915c9ecfd08ae4f90b37c51
Schala <schalaalexiazeal@gmail.com>
2022-06-02 15:33:05 -0700

    Update Dial.cs

--- Assets/Scripts/Dial.cs
+++ Assets/Scripts/Dial.cs
@@ -12,12 +12,16 @@ public class Dial : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-		if (!Input.GetMouseButtonDown(0)) return;
+		if (!Input.GetMouseButtonDown(0)) return; // is the mouse button even pressed?
 		mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if (!Physics.Raycast(mouseRay, out _)) return;
+		if (!Physics.Raycast(mouseRay, out _)) return; // ... or on the dial, for that matter?
 
 		var delta = Input.mousePosition - lastMousePosition;
+
+		// atan2 will get the rotation angle for us, in radians. Rotate the dial around its parent object by that amount
 		needle.transform.RotateAround(transform.position, needle.transform.forward, (Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg));
+		
+		// update the last mouse position
 		lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 	}
 }
